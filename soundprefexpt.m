b8a7b8d7f828a771e974a6999cf13af98aec30e2
Miren <Miren@MirenComp.local>
2015-11-04 23:15:56 -0800

    Main Experiment File Created

---
+++ soundprefexpt.m
@@ -0,0 +1,10 @@
+%% Sound Preferences Experiment
+
+%Drain dripping vs rain. Why are isolated sounds coming from a
+%single source more annoying? Are there just some sounds that people
+%dislike? Does this overlap with miso sounds? Is it a spectrum? Gender diffs?
+%What are the underlying traits of sounds that people seem to dislike? Are
+%there some sounds that are pleasurable?
+
+%Design: Play two sounds at a subject, force them to choose which one they
+%would rather listen to for X amount of time. Randomly pair the sounds. 
